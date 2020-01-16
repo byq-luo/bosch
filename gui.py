852ef717a7358941e12708568a2cd30165c136f0
gengelb2@msu.edu <Papertower97>
2020-01-16 17:03:20 -0500

    added dummy tkinter window to test working

---
+++ gui.py
@@ -0,0 +1,12 @@
+from tkinter import *
+import cv2
+
+class App:
+    def __init__(self):
+        window = Tk()
+        window.title("Bosch Interactive Cruise Control")
+        window.mainloop()
+
+
+start = App()
+
