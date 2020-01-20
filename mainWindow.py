7a61ad6fbe140f15ca1036ccd8360d1c6ec7f508
James Gengelbach <gengelb2@msu.edu>
2020-01-20 13:57:30 -0500

    the video runs in the main window. continually pressing play breaks it

--- mainWindow.py
+++ mainWindow.py
@@ -3,6 +3,8 @@ from tkinter import ttk
 from tkinter import filedialog
 import gui
 import pathlib
+import PIL.Image, PIL.ImageTk
+
 
 
 class Root(Tk):
@@ -12,6 +14,7 @@ class Root(Tk):
         self.title("Main Window")
         self.minsize(640, 400)
         self.filename = None
+        self.vid = None
         self.wm_iconbitmap('icon2.ico')
         # The frame for file button and file path
         self.FileFrame = ttk.LabelFrame(self, text="Open File")
@@ -22,6 +25,17 @@ class Root(Tk):
         self.VideoFrame = ttk.LabelFrame(self, text="Video")
         self.VideoFrame.grid(column=1, row=1, padx=20, pady=20)
         self.buttonVideo()
+        #self.appcanvas = Canvas(self, width=500, height=500)
+
+
+        #The video widget
+        self.videoLabel = ttk.Label(self, anchor=S, image=None)
+        self.videoLabel.grid(column=1, row=2,)
+
+        #
+        self.delay = 15
+        #self.update()
+
 
     def buttonFile(self):
         self.filebutton = ttk.Button(self.FileFrame, text="Browse A File", command=self.fileDialog)
@@ -40,7 +54,22 @@ class Root(Tk):
         # App(Tk(), "Tkinter and OpenCV", self.filename)
 
     def makeVideo(self):
-        gui.App(Tk(), "Tkinter and OpenCV", self.filename)
+        #gui.App(Tk(), "Tkinter and OpenCV", self.filename)
+        self.vid = gui.Video(self.filename)
+        self.update()
+
+    def update(self):
+        if self.vid is not None:
+            ret, frame = self.vid.get_frame()
+
+            if ret:
+                photo = PIL.ImageTk.PhotoImage(image=PIL.Image.fromarray(frame))
+                self.videoLabel.photo = photo
+                self.videoLabel.config(image=photo)
+
+
+
+        self.after(self.delay, self.update)
 
 
 
