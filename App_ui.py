3d3820419cca9392f27075fb730658f073ce4b1c
schrotha <schrotha@msu.edu>
2020-02-03 17:24:52 -0500

    add pause button

--- App_ui.py
+++ App_ui.py
@@ -12,23 +12,35 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
+
+        '''Make the main window'''
         MainWindow.setObjectName("MainWindow")
         MainWindow.resize(1572, 675)
+
+        '''Set up the central widget'''
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
+
+        '''Create the widget layouts'''
         self.horizontalLayout = QtWidgets.QHBoxLayout(self.centralwidget)
         self.horizontalLayout.setObjectName("horizontalLayout")
         self.verticalLayout_2 = QtWidgets.QVBoxLayout()
         self.verticalLayout_2.setObjectName("verticalLayout_2")
         self.label_2 = QtWidgets.QLabel(self.centralwidget)
         self.label_2.setObjectName("label_2")
+
+        '''Add the file list widget'''
         self.verticalLayout_2.addWidget(self.label_2)
         self.fileListWidget = QtWidgets.QListWidget(self.centralwidget)
         self.fileListWidget.setObjectName("fileListWidget")
         self.verticalLayout_2.addWidget(self.fileListWidget)
         self.horizontalLayout.addLayout(self.verticalLayout_2)
+
+        '''Add a spacer item'''
         spacerItem = QtWidgets.QSpacerItem(20, 20, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Minimum)
         self.horizontalLayout.addItem(spacerItem)
+
+        '''Add the video widget and set its size'''
         self.verticalLayout_6 = QtWidgets.QVBoxLayout()
         self.verticalLayout_6.setObjectName("verticalLayout_6")
         self.videoWidget = VideoWidget(self.centralwidget)
@@ -40,28 +52,42 @@ class Ui_MainWindow(object):
         self.videoWidget.setMinimumSize(QtCore.QSize(10, 0))
         self.videoWidget.setObjectName("videoWidget")
         self.verticalLayout_6.addWidget(self.videoWidget)
+
+        '''Add the play button'''
         self.horizontalLayout_2 = QtWidgets.QHBoxLayout()
         self.horizontalLayout_2.setObjectName("horizontalLayout_2")
         self.playButton = QtWidgets.QPushButton(self.centralwidget)
         self.playButton.setObjectName("playButton")
         self.horizontalLayout_2.addWidget(self.playButton)
+
+        '''Add a spacer item'''
         spacerItem1 = QtWidgets.QSpacerItem(20, 20, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Minimum)
         self.horizontalLayout_2.addItem(spacerItem1)
+
+        '''Add a horizontal slider'''
         self.horizontalSlider = QtWidgets.QSlider(self.centralwidget)
         self.horizontalSlider.setOrientation(QtCore.Qt.Horizontal)
         self.horizontalSlider.setInvertedAppearance(False)
         self.horizontalSlider.setInvertedControls(False)
         self.horizontalSlider.setObjectName("horizontalSlider")
         self.horizontalLayout_2.addWidget(self.horizontalSlider)
+
+        '''Add a spacer item'''
         spacerItem2 = QtWidgets.QSpacerItem(20, 20, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Minimum)
         self.horizontalLayout_2.addItem(spacerItem2)
+
+        '''Add the pause button'''
         self.pauseButton = QtWidgets.QPushButton(self.centralwidget)
         self.pauseButton.setObjectName("pauseButton")
         self.horizontalLayout_2.addWidget(self.pauseButton)
         self.verticalLayout_6.addLayout(self.horizontalLayout_2)
         self.horizontalLayout.addLayout(self.verticalLayout_6)
+
+        '''Add a spacer item'''
         spacerItem3 = QtWidgets.QSpacerItem(20, 20, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Minimum)
         self.horizontalLayout.addItem(spacerItem3)
+
+        '''Add label list widget'''
         self.verticalLayout = QtWidgets.QVBoxLayout()
         self.verticalLayout.setObjectName("verticalLayout")
         self.label = QtWidgets.QLabel(self.centralwidget)
@@ -70,38 +96,62 @@ class Ui_MainWindow(object):
         self.labelListWidget = QtWidgets.QListWidget(self.centralwidget)
         self.labelListWidget.setObjectName("labelListWidget")
         self.verticalLayout.addWidget(self.labelListWidget)
+
+        '''Add showLabels checkbox'''
         self.showLabelsCheckbox = QtWidgets.QCheckBox(self.centralwidget)
         self.showLabelsCheckbox.setObjectName("showLabelsCheckbox")
         self.verticalLayout.addWidget(self.showLabelsCheckbox)
+
+        '''Add showBoundingBox checkbox'''
         self.boundingBoxCheckbox = QtWidgets.QCheckBox(self.centralwidget)
         self.boundingBoxCheckbox.setObjectName("boundingBoxCheckbox")
         self.verticalLayout.addWidget(self.boundingBoxCheckbox)
         self.horizontalLayout.addLayout(self.verticalLayout)
+
+        '''Set central widget'''
         MainWindow.setCentralWidget(self.centralwidget)
+
+        '''Add status bar'''
         self.statusbar = QtWidgets.QStatusBar(MainWindow)
         self.statusbar.setObjectName("statusbar")
         MainWindow.setStatusBar(self.statusbar)
+
+        '''Add tool bar'''
         self.toolBar = QtWidgets.QToolBar(MainWindow)
         self.toolBar.setObjectName("toolBar")
         MainWindow.addToolBar(QtCore.Qt.TopToolBarArea, self.toolBar)
+
+        '''Add file select option to toolbar'''
         self.processOneFileAction = QtWidgets.QAction(MainWindow)
         icon = QtGui.QIcon()
         icon.addPixmap(QtGui.QPixmap("icons/file.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
         self.processOneFileAction.setIcon(icon)
         self.processOneFileAction.setText("")
         self.processOneFileAction.setObjectName("processOneFileAction")
+
+        '''Add folder select option to toolbar'''
         self.processMultipleFilesAction = QtWidgets.QAction(MainWindow)
         icon1 = QtGui.QIcon()
         icon1.addPixmap(QtGui.QPixmap("icons/folder.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
         self.processMultipleFilesAction.setIcon(icon1)
         self.processMultipleFilesAction.setText("")
         self.processMultipleFilesAction.setObjectName("processMultipleFilesAction")
+
+        '''Add action to play button'''
         self.playAction = QtWidgets.QAction(MainWindow)
         self.playAction.setObjectName("playAction")
+
+        '''Add action to pause button'''
         self.pauseAction = QtWidgets.QAction(MainWindow)
         self.pauseAction.setObjectName("pauseAction")
+
+        '''Add action to file select'''
         self.toolBar.addAction(self.processOneFileAction)
+
+        '''Add tool bar separator'''
         self.toolBar.addSeparator()
+
+        '''Add action to folder select'''
         self.toolBar.addAction(self.processMultipleFilesAction)
 
         self.retranslateUi(MainWindow)
