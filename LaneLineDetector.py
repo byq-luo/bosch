51afcb712c5bcfb568b7ae965006bdcc5d2abc0f
Tianlun Chen <chenti29@msu.edu>
2020-02-16 23:16:25 -0500

    Update lane detection

--- LaneLineDetector.py
+++ LaneLineDetector.py
@@ -145,3 +145,91 @@ class LaneLineDetector:
     transformed = cv2.warpPerspective(frame, m, (width, height), flags=cv2.INTER_LINEAR)
 
     return transformed
+
+  # 5 way to process the frame and get information
+  def _abs_sobel_thresh(self, img, orient='x', thresh_min=0, thresh_max=255):
+    # Convert to grayscale
+    gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+    # Apply x or y gradient with the OpenCV Sobel() function
+    # and take the absolute value
+    if orient == 'x':
+      abs_sobel = np.absolute(cv2.Sobel(gray, cv2.CV_64F, 1, 0))
+    elif orient == 'y':
+      abs_sobel = np.absolute(cv2.Sobel(gray, cv2.CV_64F, 0, 1))
+    else:
+      abs_sobel = np.absolute(cv2.Sobel(gray, cv2.CV_64F, 1, 0))
+    # Rescale back to 8 bit integer
+    scaled_sobel = np.uint8(255*abs_sobel/np.max(abs_sobel))
+    # Create a copy and apply the threshold
+    binary_output = np.zeros_like(scaled_sobel)
+    # Here I'm using inclusive (>=, <=) thresholds, but exclusive is ok too
+    binary_output[(scaled_sobel >= thresh_min) & (scaled_sobel <= thresh_max)] = 1
+
+    # Return the result
+    return binary_output
+
+  def _mag_threshold(self, img, sobel_kernel=3, mag_thresh=(0, 255)):
+    # Convert to grayscale
+    gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+    # Take both Sobel x and y gradients
+    sobelx = cv2.Sobel(gray, cv2.CV_64F, 1, 0, ksize=sobel_kernel)
+    sobely = cv2.Sobel(gray, cv2.CV_64F, 0, 1, ksize=sobel_kernel)
+    # Calculate the gradient magnitude
+    gradmag = np.sqrt(sobelx**2 + sobely**2)
+    # Rescale to 8 bit
+    scale_factor = np.max(gradmag)/255
+    gradmag = (gradmag/scale_factor).astype(np.uint8)
+    # Create a binary image of ones where threshold is met, zeros otherwise
+    binary_output = np.zeros_like(gradmag)
+    binary_output[(gradmag >= mag_thresh[0]) & (gradmag <= mag_thresh[1])] = 1
+
+    # Return the binary image
+    return binary_output
+
+  def _dir_threshold(self, img, sobel_kernel=3, thresh=(0, np.pi/2)):
+    # Grayscale
+    gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+    # Calculate the x and y gradients
+    sobelx = cv2.Sobel(gray, cv2.CV_64F, 1, 0, ksize=sobel_kernel)
+    sobely = cv2.Sobel(gray, cv2.CV_64F, 0, 1, ksize=sobel_kernel)
+    # Take the absolute value of the gradient direction,
+    # apply a threshold, and create a binary image result
+    absgraddir = np.arctan2(np.absolute(sobely), np.absolute(sobelx))
+    binary_output =  np.zeros_like(absgraddir)
+    binary_output[(absgraddir >= thresh[0]) & (absgraddir <= thresh[1])] = 1
+
+    # Return the binary image
+    return binary_output
+
+  def _hls_select(self, img,channel='s',thresh=(0, 255)):
+    hls = cv2.cvtColor(img, cv2.COLOR_RGB2HLS)
+    if channel=='h':
+      channel = hls[:,:,0]
+    elif channel=='l':
+      channel=hls[:,:,1]
+    else:
+      channel=hls[:,:,2]
+    binary_output = np.zeros_like(channel)
+    binary_output[(channel > thresh[0]) & (channel <= thresh[1])] = 1
+    return binary_output
+
+  def _luv_select(self, img, thresh=(0, 255)):
+    luv = cv2.cvtColor(img, cv2.COLOR_RGB2LUV)
+    l_channel = luv[:,:,0]
+    binary_output = np.zeros_like(l_channel)
+    binary_output[(l_channel > thresh[0]) & (l_channel <= thresh[1])] = 1
+    return binary_output
+
+  def _thresholding(self, img):
+    #setting all sorts of thresholds
+    x_thresh = self._abs_sobel_thresh(img, orient='x', thresh_min=70 ,thresh_max=255)
+    mag_thresh = self._mag_threshold(img, sobel_kernel=3, mag_thresh=(30, 170))
+    dir_thresh = self._dir_threshold(img, sobel_kernel=3, thresh=(0.7, 1.5))
+    hls_thresh = self._hls_select(img, thresh=(20, 255))
+    luv_thresh = self._luv_select(img, thresh=(200, 255))
+
+    #Thresholding combination
+    threshholded = np.zeros_like(x_thresh)
+    threshholded[((x_thresh == 1) & (mag_thresh == 1)) | ((dir_thresh == 1) & (hls_thresh == 1)) | (luv_thresh == 1)] = 1
+
+    return threshholded
