4352aba23a1507cfd19c140c0def93d68a33aff5
schrotha <schrotha@msu.edu>
2020-02-19 10:14:06 -0500

    Label generation for rightTO, evtEnd, and cutout

--- LabelGenerator.py
+++ LabelGenerator.py
@@ -5,8 +5,9 @@ class LabelGenerator:
     self.currentTargetObject = None
     self.newPotentialTarget = None
     self.newEventTimer = 10
-    self.newTargetTimer = 10
     self.lastLabelProduced = None
+    self.targetDirection = None
+    self.lastTargetPos = None
     self._time = None
     self.labels = []
     self.videoFPS = videoFPS
@@ -15,8 +16,6 @@ class LabelGenerator:
       return self.labels
 
   def processFrame(self, vehicles, lines):
-    # # TODO we can the equations from the LandLineDetector.
-    # if len(lines) > 10000: # for code folding in the editor
 
     leftXB = lines[0][0]
     leftYB = lines[0][1]
@@ -39,11 +38,11 @@ class LabelGenerator:
     #     detect boxes that are half in the lane on left and right
     #     detect boxes completely out of lane on left and right
 
-    boxesOutLaneLeft = []
-    boxesOnLeftLane = []
-    boxesInLane = []
-    boxesOnRightLane = []
-    boxesOutLaneRight = []
+    vehiclesOutLaneLeft = []
+    vehiclesOnLeftLane = []
+    vehiclesInLane = []
+    vehiclesOnRightLane = []
+    vehiclesOutLaneRight = []
 
     boxIndex = 0
 
@@ -77,50 +76,153 @@ class LabelGenerator:
         rInsideRightEdge = True
 
       if lInsideLeftEdge and rInsideRightEdge:
-        boxesInLane.append(box)
+        vehiclesInLane.append(vehicle)
 
       if not lInsideLeftEdge and rInsideLeftEdge:
-        boxesOnLeftLane.append(box)
+        vehiclesOnLeftLane.append(vehicle)
 
       if not lInsideLeftEdge and not rInsideLeftEdge:
-        boxesOutLaneLeft.append(box)
+        vehiclesOutLaneLeft.append(vehicle)
 
       if not rInsideRightEdge and lInsideRightEdge:
-        boxesOnRightLane.append(box)
+        vehiclesOnRightLane.append(vehicle)
 
       if not rInsideRightEdge and not lInsideRightEdge:
-        boxesOutLaneRight.append(box)
+        vehiclesOutLaneRight.append(vehicle)
 
-    # Actually produce the labels
-    # THINGS TO DO IN THIS SECTION:
-    #     Use ID of current Target object to find out which list its in:
-    #     The first _time the target object leaves the lane, begin decreasing the new event timer
-    #     If the new event timer reaches 0 and the target object has not returned to the lane, produce a new label
-    #     Once the target object leaves the lane, start the new event timer
-    #     When timer reaches 0 produce evtEnd label and set currentTargetObject to None
 
-    # This section is used to determine the targetObject
-    if currentTargetObject is None:
-      y = 0
-      targetFound = False
-      for box in boxesInLane:
-        # finds the closest target to the host vehicle
-        if box[3] > y:
-          currentTargetObjectBox = box
-          targetFound = True
+    '''
+    Finds the closest vehicle in our lane
+    '''
+    y = 0
+    closestTarget = None
+    for vehicle in vehiclesInLane:
+      # finds the closest target to the host vehicle
+      if vehicle.box[3] > y:
+        closestTarget = vehicle
 
-      if targetFound:
+    '''
+    Sets a target object if there is no current target object
+    Otherwise checks that the current target object is still the 
+    closest vehicle in lane
+    '''
+    targetInLane = False
+    if self.currentTargetObject is None:
+      if closestTarget is not None:
+        self.currentTargetObject = closestTarget
         newLabel = ("rightTO", self._time)
         self.labels.append(newLabel)
-        lastLabelProduced = "rightTO"
+        self.lastLabelProduced = "rightTO"
+        self.lastTargetPos = "In Lane"
+        targetInLane = True
+    else:
+      if self.currentTargetObject.id == closestTarget.id:
+        targetInLane = True
 
-    if lastLabelProduced == "rightTO":
-      pass
+    '''
+    This section handles when a target object is in the host vehicle lane
+    '''
+    if self.lastLabelProduced == "rightTO":
 
-    if lastLabelProduced == "objTurnOff":
-      # Check how much time is left on new event timer
-      # Also check that box is still on one of the lanes
-      pass
+      # Current target object is closest in lane
+      if targetInLane:
+        self.newEventTimer = 10
+        self.lastTargetPos = "In Lane"
+        return
 
-    if lastLabelProduced == "evtEnd":
-      pass
+      # if this is the first time it has left the host lane
+      if self.newEventTimer == 10:
+        self.newEventTimer = 9
+        for vehicle in vehiclesOnLeftLane:
+          if vehicle.id == self.currentTargetObject.id:
+            self.targetDirection = "Left"
+            return
+
+        for vehicle in vehiclesOnRightLane:
+          if vehicle.id == self.currentTargetObject.id:
+            self.targetDirection = "Right"
+            return
+
+      # if the target object has already started leaving the lane
+      elif self.newEventTimer > 0 and self.newEventTimer < 10:
+        if self.targetDirection == "Left":
+          for vehicle in vehiclesOnLeftLane:
+            if vehicle.id == self.currentTargetObject.id:
+              self.newEventTimer -= 1
+              return
+
+        if self.targetDirection == "Right":
+          for vehicle in vehiclesOnRightLane:
+            if vehicle.id == self.currentTargetObject.id:
+              self.newEventTimer -= 1
+              return
+
+      # if the target has left the lane
+      else:
+        newLabel = ("cutout", self._time)
+        self.labels.append(newLabel)
+        self.lastLabelProduced = "cutout"
+        self.newEventTimer = 10
+        if self.targetDirection == "Left":
+          self.lastTargetPos = "On Left Line"
+        if self.targetDirection == "Right":
+          self.lastTargetPos = "On Right Line"
+        return
+
+
+    '''
+    This Section handles when the current target object has cut out of 
+    the host vehicles lane
+    '''
+    if self.lastLabelProduced == "cutout":
+      stillOnEdge = False
+      inOutsideLane = False
+      if self.lastTargetPos == "On Left Line":
+        for vehicle in vehiclesOnLeftLane:
+          if vehicle.id == self.currentTargetObject.id:
+            stillOnEdge = True
+        for vehicle in vehiclesOutLaneLeft:
+          if vehicle.id == self.currentTargetObject.id:
+            inOutsideLane = True
+
+      if self.lastTargetPos == "On Right Line":
+        for vehicle in vehiclesOnRightLane:
+          if vehicle.id == self.currentTargetObject.id:
+            stillOnEdge = True
+        for vehicle in vehiclesOutLaneRight:
+          if vehicle.id == self.currentTargetObject.id:
+            inOutsideLane = True
+
+      # Object is still on the lane line
+      if stillOnEdge:
+        self.newEventTimer = 10
+        return
+
+      # Object is in neighbor lane
+      if inOutsideLane:
+        if self.newEventTimer > 0:
+          self.newEventTimer -= 1
+          return
+
+        else:
+          newLabel = ("evtEnd", self._time)
+          self.labels.append(newLabel)
+          self.lastLabelProduced = "evtEnd"
+          self.newEventTimer = 10
+          self.currentTargetObject = None
+          self.lastTargetPos = None
+          self.targetDirection = None
+          return
+
+      if targetInLane:
+        if self.newEventTimer > 0:
+          self.newEventTimer -= 1
+          return
+
+        else:
+          self.newEventTimer = 10
+          del self.labels[-1]
+          self.lastLabelProduced = "rightTO"
+          self.lastTargetPos = "In Lane"
+          self.targetDirection = None
+
