5cd49bb103886418db3f70ed78c1da1b1f0d7964
xdaimon <gazingatstars.o7@gmail.com>
2020-03-09 15:51:25 -0400

    Small change to make TESTING work again..

--- precomputed/LaneLineDetector.py
+++ precomputed/LaneLineDetector.py
@@ -1,9 +1,15 @@
 import pickle
 
 class LaneLineDetector:
-  def getLines(self, frame, frameIndex):
-    return self.lines[frameIndex]
+  def __init__(self):
+    self.frameIndex = 0
+
+  def getLines(self, frame):
+    lines = self.lines[self.frameIndex % len(self.lines)]
+    self.frameIndex += 1
+    return lines
 
   def loadFeaturesFromDisk(self, featuresPath):
+    self.frameIndex  = 0
     with open(featuresPath, 'rb') as file:
       _, _, self.lines, _ = pickle.load(file)
