a5ad819f18ee8ed9bcc5f204e86ba1276384b6ca
xdaimon <gazingatstars.o7@gmail.com>
2020-03-12 21:57:36 -0400

    update stuff.

--- lstm.py
+++ lstm.py
@@ -1,9 +1,12 @@
+# This code is junk i know sorry
+
 import os
 import pickle
 import numpy as np
 import random
 import torch
 import torch.nn as nn
+from torch.nn.utils.rnn import pad_sequence
 import torch.nn.functional as F
 import torch.optim as optim
 torch.manual_seed(1)
@@ -15,28 +18,16 @@ torch.manual_seed(1)
 # 4 : evtEnd
 # 5 : objTurnOff
 # 6 : end
-# 7 : barrier
+# 7 : barrier # TODO consider removing this
 # 8 : NOLABEL
-
-# TODO convert the object ID into a categorical input? onehot? modulo?
-# TODO sort the input tensors by objectid?
-# TODO use minibatch?
-# TODO regularization?
-
-# Doing input summarization inline with label predictions inflates the
-# number of timesteps the network has to work across.
-# If there were less timesteps then we could increase the time window
-# that the network has access to
 
 # It would be nice if we could increase the window of the label predictions
 # A problem is that having to choose the correct label on the precise frame number
 # is too demanding of the network. The exact frame number is noisy and is a human
 # choice. But the frame number plus or minus 15 frames is less subject to human bias.
 
-# Use attention?
-# two lstms. an encoder that takes bboxes and other features aand outputs a single fixed size vector.
-# and the other that has 1 timestep per frame of video and consumes the encoder's output
-
+# Use attention? bidirectional lstm?
+# how many lstm layers to use? dont wanna overfit
 
 def clamp(l, h, x):
   if x < l:
@@ -53,19 +44,28 @@ class mylstm(nn.Module):
   def __init__(self, hidden_dim, input_dim, output_size):
     super(mylstm, self).__init__()
     self.hidden_dim = hidden_dim
+    self.input_dim = input_dim
 
-    # The LSTM takes word embeddings as inputs, and outputs hidden states
-    # with dimensionality hidden_dim.
-    self.lstm = nn.LSTM(input_dim, hidden_dim, num_layers=4,)
+    # context_seq = (len(frames), 1, hiddensize)
+    # for each frame in frames:
+    #   frame is [features for each vehicle] with shape (feature_seq_len, 1, insize)
+    #   run encoder on the frame sequence to give shape (1, 1, hiddensize)
+    #   set context_seq_i = encder output
+    # compute lstm(context_seq)
 
-    # The linear layer that maps from hidden state space to tag space
+    self.encoder = nn.LSTM(input_dim, hidden_dim, num_layers=2)
+    self.lstm = nn.LSTM(hidden_dim, hidden_dim, num_layers=2)
     self.hidden2out = nn.Linear(hidden_dim, output_size)
 
-  def forward(self, inputs):
-    # add the batch dimension
-    lstm_out, _ = self.lstm(inputs)
-    # lstm_out, self.hidden = self.lstm(inputs, self.hidden)
-    out_space = self.hidden2out(lstm_out.view(len(inputs), -1))
+  def forward(self, data):
+    # see nn.LSTM documentation for input and output shapes
+    context_seq = self.encoder(data) # == h_T, (hs, cs)
+    context_seq = context_seq[1] # == (hs,cs)
+    context_seq = context_seq[0] # == hs
+    context_seq = context_seq[-1] # == hs for last layer, i think
+    context_seq = context_seq.view(context_seq.shape[0], 1, context_seq.shape[1])
+    lstm_out, _ = self.lstm(context_seq)
+    out_space = self.hidden2out(lstm_out.view(lstm_out.shape[0], -1))
     out_scores = F.log_softmax(out_space, dim=1)
     return out_scores
 
@@ -75,64 +75,53 @@ def train(sequences):
   model = mylstm(2*17, 17, NUMLABELS)
   model.to(torch.device('cuda'))
   model.train()
-  loss_function = nn.NLLLoss()
+
+  # 0 : rightTO
+  # 1 : lcRel
+  # 2 : cutin
+  # 3 : cutout
+  # 4 : evtEnd
+  # 5 : objTurnOff
+  # 6 : end
+  # 7 : barrier
+  # 8 : NOLABEL
+  class_weights = torch.tensor([1/1400,1/600,1/200,1/380,1/1200,1/50,1/170,1/90,1/8000],device=torch.device('cuda'))/0.04796432871394172
+  # class_weights = torch.tensor([1400/4140,600/4140,200/4140,380/4140,1200/4140,50/4140,170/4140,90/4140,50/4140],device=torch.device('cuda'))
+
+  # loss_function = nn.CrossEntropyLoss(weight=class_weight)
+  loss_function = nn.NLLLoss(weight=class_weights)
   # optimizer = optim.SGD(model.parameters(), lr=0.1)
   optimizer = optim.Adam(model.parameters())
 
-  tensors = sequences[0][0].to(torch.device('cuda'))
-  labels = sequences[0][1].to(torch.device('cuda'))
-
-  print('Get initial model outputs')
-  # See what the scores are before training
-  # Note that element i,j of the output is the score for tag j for word i.
-  # Here we don't need to train, so the code is wrapped in torch.no_grad()
-  with torch.no_grad():
-    model.eval()
-    yhat = model(tensors)
-    model.train()
-
   print('Enter training loop')
-  loss = None
+  ema_loss = 1
   losses = []
   for epoch in range(5000):
-    print(epoch, '/', 5000, '  ', loss)
-
-    if epoch % 1000 == 999:
+    if epoch % 100 == 0 and epoch > 0:
       with torch.no_grad():
         model.eval()
         print('Saving model at epoch:',epoch)
         torch.save(model.state_dict(), 'model.pt')
-        for x, y in sequences:
-          x = x.to(torch.device('cuda'))
-          y = y.to(torch.device('cuda'))
-          # Step 1. Remember that Pytorch accumulates gradients.
-          # We need to clear them out before each instance
+        for x, y in sequences[:20]:
           model.zero_grad()
-
-          # Step 3. Run our forward pass.
           yhat = model(x)
           yhat = yhat.argmax(dim=1)
           print('yhat:', yhat)
           print('y   :', y)
         model.train()
+        with open('losses.pkl', 'wb') as file:
+          pickle.dump(losses, file)
 
-    model.train()
-    for x, y in sequences:
-      x = x.to(torch.device('cuda'))
-      y = y.to(torch.device('cuda'))
-      # Step 1. Remember that Pytorch accumulates gradients.
-      # We need to clear them out before each instance
-      model.zero_grad()
-
-      # Step 3. Run our forward pass.
-      yhat = model(x)
-
-      # Step 4. Compute the loss, gradients, and update the parameters by
-      #  calling optimizer.step()
-      loss = loss_function(yhat, y)
+    for i,(x, y) in enumerate(sequences):
+      loss = loss_function(model(x), y)
       losses.append(float(loss))
       loss.backward()
-      optimizer.step()
+      if i % 10 == 0: # use minibatches of size 10
+          optimizer.step()
+          model.zero_grad()
+
+    ema_loss = .6 * ema_loss + .4 * losses[-1]
+    print(epoch, '/', 5000, '  ', ema_loss)
 
   with open('losses.pkl', 'wb') as file:
     pickle.dump(losses, file)
@@ -141,13 +130,7 @@ def train(sequences):
   model.eval()
   with torch.no_grad():
     for x, y in sequences:
-      x = x.to(torch.device('cuda'))
-      y = y.to(torch.device('cuda'))
-      # Step 1. Remember that Pytorch accumulates gradients.
-      # We need to clear them out before each instance
       model.zero_grad()
-
-      # Step 3. Run our forward pass.
       yhat = model(x)
       yhat = yhat.argmax(dim=1)
       print('yhat:', yhat)
@@ -167,10 +150,14 @@ if __name__ == '__main__':
   # # Collect labels and features into one place
   # data = []  # == [path, [label, framnum], features] where features == [rawboxes, boxscores, lines, lanescores, vehicles, boxcornerprobs]
   # for filepath in filepaths:
+  #   labelsFilePath = filepath.replace('features/', 'groundTruthLabels/').replace('_m0.pkl', '_labels.txt')
+  #
+  #   if not os.path.isfile(labelsFilePath):
+  #     continue
+  #
   #   videodata = [filepath]
   #
   #   # Get labels
-  #   labelsFilePath = filepath.replace('features/', 'groundTruthLabels/').replace('_m0.pkl', '_labels.txt')
   #   with open(labelsFilePath) as labelsFile:
   #     labels = []
   #     lines = labelsFile.readlines()
@@ -196,8 +183,6 @@ if __name__ == '__main__':
   # for label in AllPossibleLabels:
   #   labels2Tensor[label] = torch.tensor([len(labels2Tensor)])
   #
-  # ENDSIGNAL = torch.tensor([[[-1]*17]], dtype=torch.float)  # size = (1,1,17)
-  #
   # # Make input tensors from the data
   # sequences = []
   # for path, labels, features in data:
@@ -209,9 +194,10 @@ if __name__ == '__main__':
   #     left = i
   #     right = i + intervalwidth
   #
-  #     # TODO i make the assumption that there is only 1 label per frame
   #     frameNum2LabelTensors = {}
   #     for label, frameNum in labels:
+  #       if frameNum in frameNum2LabelTensors:
+  #         frameNum += 1
   #       frameNum2LabelTensors[frameNum] = labels2Tensor[label]
   #
   #     # Now for each frame in the interval
@@ -226,6 +212,7 @@ if __name__ == '__main__':
   #         probs = boxcornerprobs[i:i+intervalwidth+1]
   #         lanescores = lanescores[i:i+intervalwidth+1]
   #
+  #         # for each frame in the interval
   #         for j, (vehicles, probs, scores) in enumerate(zip(vehicles, probs, lanescores)):
   #           scores = [float(score.cpu().numpy()) for score in scores]
   #
@@ -233,6 +220,7 @@ if __name__ == '__main__':
   #           probsleft = probs[:len(vehicles)]  # left lane line probability map values at box corners for each vehicle
   #           probsright = probs[len(vehicles):]  # See LaneLineDetectorERFNet.py::175
   #
+  #           features_xs = []
   #           # Create tensors
   #           for vehicle, probleft, probright in zip(vehicles, probsleft, probsright):
   #             # Put object id into sensible range
@@ -240,24 +228,26 @@ if __name__ == '__main__':
   #             objectid = (objectid % 1000) / 1000
   #             vehicle = (objectid, x1, y1, x2, y2)
   #
-  #             xs.append(torch.tensor([[[*vehicle, *probleft, *probright, *scores]]], dtype=torch.float))
-  #             ys.append(labels2Tensor['NOLABEL'])
+  #             features_xs.append(torch.tensor([[*vehicle, *probleft, *probright, *scores]], dtype=torch.float))
+  #           if len(features_xs) == 0:
+  #             features_xs = [torch.tensor([ [0]*13 + [*scores] ], dtype=torch.float)] # make sure there is always an input tensor
+  #           features_xs = torch.cat(features_xs)
   #
-  #           # entice the network to produce a label
-  #           xs.append(ENDSIGNAL)
+  #           xs.append(features_xs)
   #           if i+j in frameNum2LabelTensors:
   #             ys.append(frameNum2LabelTensors[i+j])
   #           else:
   #             ys.append(labels2Tensor['NOLABEL'])
   #
-  #         xs = torch.cat(xs).to(torch.device('cuda'))
+  #         xs = pad_sequence(xs).to(torch.device('cuda'))
+  #         xs = torch.flip(xs,dims=(0,))
   #         ys = torch.cat(ys).to(torch.device('cuda'))
   #         sequences.append((xs, ys))
   #
   #         # There may be more than one label in this interval.
   #         # If we ran this loop twice in this interval then we would append the same exact (xs,ys) to sequences
   #         break
-  #
+
   # print(len(sequences))
   # with open('tensors.pkl', 'wb') as file:
   #   pickle.dump(sequences, file)
