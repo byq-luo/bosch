3a630d3e63bdd77a4d83412d144801a65c5e3a03
sabrinaga <garci353@msu.edu>
2020-03-15 01:15:33 -0400

    confidence interval test

--- ConfidenceIntervals.py
+++ ConfidenceIntervals.py
@@ -1,5 +1,18 @@
-# accuracy = total correct predictions / total predictions made * 100
-# binomial confidence interval
 from math import sqrt
-interval = 1.96 * sqrt( (0.2 * (1 - 0.2)) / 50)
-print('%.3f' % interval)
+
+
+def calculate_accuracy(predictedLabel, correctLabel, predictedLabelCount):
+    accuracyCouter = 0
+    if predictedLabel == correctLabel:
+        accuracyCouter += 1
+    accuracy = (accuracyCouter / predictedLabelCount) * 100
+    return accuracy
+
+
+# accuracy = total correct predictions / total predictions made * 100
+accuracy = .10
+z = 1.64  #number of standard deviations from the gaussian distribution (90%)
+n = 50 #sample size
+interval = z * sqrt( (accuracy * (1 - accuracy)) / n)  # binomial confidence interval
+
+print("Interval: ", interval)
